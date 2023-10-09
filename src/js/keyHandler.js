c58ae4f92d5523f47e208e0d297f8c2ecafb069a
Антон Епифанов <antonepifanov87@gmail.com>
2023-10-09 23:29:29 +0300

    module2

---
+++ src/js/keyHandler.js
@@ -0,0 +1,18 @@
+const keyHandler = () => {
+  const elem = document.createElement('span');
+  elem.classList.add('.hit');
+  document.body.appendChild(elem);
+
+  let result = 0;
+
+  document.body.onkeyup = (evt) => {
+    // eslint-disable-next-line no-constant-condition
+    if ((evt.code === 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) {
+      result++;
+    }
+
+    elem.innerHTML = result;
+  };
+};
+
+export default keyHandler;
